ebff145834f4a721100b76ec91c6f7c6c0337720
Jinashree Gane <jinashrigane45@gmail.com>
2023-04-22 21:37:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Button from './Components/Button.js'
+
+function App(){
+  return (
+    <div>
+      <button>Apple</button>
+      <button>Mango</button>
+    </div>
+  )
+}
+export default App
